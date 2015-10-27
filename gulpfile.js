d9ca95577a0432b5d87a5517a965bd80d7ead0a3
wang <i@dearmadman.com>
2015-10-27 15:59:24 +0800

    add usemin and rev

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,46 @@ var minifyCss = require('gulp-minify-css');
 var minifyHtml = require('gulp-minify-html');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var clean=require('gulp-clean');
-var plumber=require('gulp-plumber');
-var extreplace=require('gulp-ext-replace');
-var babel=require('gulp-babel');
+var clean = require('gulp-clean');
+var plumber = require('gulp-plumber');
+var extreplace = require('gulp-ext-replace');
+var babel = require('gulp-babel');
+var usermin = require('gulp-usemin');
+var rev = require('gulp-rev');
 
 
 
 gulp.task('html', function() {
     console.log('moving html start...');
     gulp.src('app/*.html')
-        .pipe(minifyHtml())
+        .pipe(usemin({
+            css: [rev()],
+            html: [minifyHtml({
+                empty: true
+            })],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [minifyCss(), 'concat']
+        }))
         .pipe(gulp.dest('dist'));
     console.log('moving html end...');
 });
 
-gulp.task('asset',function(){
+gulp.task('asset', function() {
     console.log('moving assets start...');
     gulp.src('app/asset/**')
-    .pipe(gulp.dest('dist/asset'));
+        .pipe(gulp.dest('dist/asset'));
     console.log('moving assets stop...');
 });
 
-gulp.task('js',function(){
+gulp.task('js', function() {
     console.log('moving js start...');
     gulp.src('app/js/**/*.js')
-    .pipe(plumber())
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(plumber.stop())
-    .pipe(gulp.dest('dist/js'));
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('sass', function() {
@@ -56,7 +66,7 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('server',function() {
+gulp.task('server', function() {
     var server = liveServer.static('dist', 3000);
     server.start();
     gulp.watch('app/*.html', ['html']);
@@ -72,15 +82,17 @@ gulp.task('server',function() {
 });
 
 
-gulp.task('clean',function(){
+gulp.task('clean', function() {
     console.log('clean start ...');
-     gulp.src('dist/**/*',{read:false})
-     .pipe(plumber())
-     .pipe(clean());
+    gulp.src('dist/**/*', {
+            read: false
+        })
+        .pipe(plumber())
+        .pipe(clean());
 })
 
 
 
-gulp.task('default', ['server','html', 'js','sass','asset'], function() {
+gulp.task('default', ['server', 'html', 'js', 'sass', 'asset'], function() {
     console.log('default task running...');
 });
